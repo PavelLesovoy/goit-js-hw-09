c7e11e96a026a5f574b4542a9eb2a84fa5d8220e
PavelLesovoy <98907227+PavelLesovoy@users.noreply.github.com>
2022-09-27 17:16:16 +0300

    q2 finished

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,4 +1,4 @@
-import {Notify} from 'notiflix/build/notiflix-notify-aio';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
@@ -23,23 +23,22 @@ const options = {
         selectedData = selectedDates[0].getTime();
 
         if (selectedData < Date.now()) {
-            Notify.failure("Please choose a date in the future");
+            Notify.failure('Please choose a date in the future');
             refs.btn.setAttribute('disabled', 'true');
-        } 
-        else {
+        } else {
             refs.btn.removeAttribute('disabled');
             Notify.success('You can set time by press START');
         }
     },
-  };
+};
 
-  flatpickr(refs.input, options);
+flatpickr(refs.input, options);
 
-  const timer = {
+const timer = {
     timerId: null,
     disable: (refs.btn.disabled = true),
 
-    start(){
+    start() {
         this.timerId = setInterval(() => {
             const currentDate = Date.now();
             const delta = selectedData - currentDate;
@@ -50,45 +49,45 @@ const options = {
             updateClockFace(time);
         }, 1_000);
     },
-  };
+};
 
-    refs.btn.addEventListener('click', () => {
+refs.btn.addEventListener('click', () => {
     timer.start();
     refs.btn.disabled = true;
     refs.input.disable = true;
 });
 
-    const addLeadingZero = value => {
-        if (value < 10) {
-            return String(value).padStart(2, '0');
-        }
-        return value;
-    };
-
-    function updateClockFace(time) {
-        refs.days.textContent = addLeadingZero(time.days);
-        refs.hours.textContent = addLeadingZero(time.hours);
-        refs.minutes.textContent = addLeadingZero(time.minutes);
-        refs.seconds.textContent = addLeadingZero(time.seconds);
+const addLeadingZero = value => {
+    if (value < 10) {
+        return String(value).padStart(2, '0');
     }
+    return value;
+};
 
-    function convertMs(ms) {
-        // Number of milliseconds per unit of time
-        const second = 1000;
-        const minute = second * 60;
-        const hour = minute * 60;
-        const day = hour * 24;
-      
-        // Remaining days
-        const days = addLeadingZero(Math.floor(ms / day));
-        // Remaining hours
-        const hours = addLeadingZero(Math.floor((ms % day) / hour));
-        // Remaining minutes
-        const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-        // Remaining seconds
-        const seconds = addLeadingZero(
-            Math.floor((((ms % day) % hour) % minute) / second)
-        );
+function updateClockFace(time) {
+    refs.days.textContent = addLeadingZero(time.days);
+    refs.hours.textContent = addLeadingZero(time.hours);
+    refs.minutes.textContent = addLeadingZero(time.minutes);
+    refs.seconds.textContent = addLeadingZero(time.seconds);
+}
 
-        return { days, hours, minutes, seconds };
-        }
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = addLeadingZero(Math.floor(ms / day));
+    // Remaining hours
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    // Remaining seconds
+    const seconds = addLeadingZero(
+        Math.floor((((ms % day) % hour) % minute) / second)
+    );
+
+    return { days, hours, minutes, seconds };
+}
